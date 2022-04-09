0c7018501968129557b4bf8185dc0224ac304d23
LucSil <luciansilva351@gmail.com>
2022-04-09 08:05:30 +0100

    imported VR.vue into Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,14 @@
-<template></template>
+<template>
+  <section class="bg-p-white px-6">
+    <VR />
+  </section>
+</template>
 
 <script>
+import VR from "./children/VR.vue";
+
 export default {
   name: "Main",
+  components: { VR },
 };
 </script>
