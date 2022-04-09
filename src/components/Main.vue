7c778f785eff6c602a4702552536ecc57a32bf9b
LucSil <luciansilva351@gmail.com>
2022-04-09 12:55:59 +0100

    created 'Creations.vue' and imported it into 'Main.vue'

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,16 @@
 <template>
   <section class="bg-p-white px-6 py-24">
     <VR />
+    <Creations />
   </section>
 </template>
 
 <script>
 import VR from "./children/VR.vue";
+import Creations from "./children/Creations.vue";
 
 export default {
   name: "Main",
-  components: { VR },
+  components: { VR, Creations },
 };
 </script>
