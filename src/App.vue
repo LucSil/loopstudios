247f1da3da6b28781d2ce24915817acf310aec68
LucSil <luciansilva351@gmail.com>
2022-04-09 07:57:21 +0100

    imported Main.vue into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Hero />
+  <Main />
 </template>
 
 <script>
 import Hero from "@/components/Hero.vue";
+import Main from "@/components/Main.vue";
 
 export default {
   name: "App",
-  components: { Hero },
+  components: { Hero, Main },
 };
 </script>
