b151656faf445869f8423ad48ac0495fa6b60e15
LucSil <luciansilva351@gmail.com>
2022-04-11 10:25:45 +0100

    responsive desktop design for Hero.vue complete

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,9 +4,9 @@
       v-if="menu"
       class="absolute inset-0 z-20 w-full h-screen bg-black"
     ></div>
-    <nav class="absolute z-30 w-full mt-10 px-6">
+    <nav class="absolute z-30 w-full mt-10 px-6 lg:px-40 lg:mt-16">
       <section class="flex justify-between items-center relative z-30">
-        <div class="w-36">
+        <div class="w-40">
           <img src="../assets/images/svg/logo.svg" alt="logo" />
         </div>
         <div class="flex justify-center items-center sm:hidden">
@@ -22,13 +22,13 @@
         </div>
         <!-- Menu shown after mobile widths -->
         <ul
-          class="sm:flex hidden text-white font-alata justify-between items-center"
+          class="sm:flex hidden text-white font-alata justify-between items-center lg:text-base"
         >
-          <li class="mainLink imp"><a href="#">ABOUT</a></li>
-          <li class="mainLink imp"><a href="#">CAREERS</a></li>
-          <li class="mainLink imp"><a href="#">EVENTS</a></li>
-          <li class="mainLink imp"><a href="#">PRODUCTS</a></li>
-          <li class="mainLink"><a href="#">SUPPORT</a></li>
+          <li class="mainLink imp"><a href="#">About</a></li>
+          <li class="mainLink imp"><a href="#">Careers</a></li>
+          <li class="mainLink imp"><a href="#">Events</a></li>
+          <li class="mainLink imp"><a href="#">Products</a></li>
+          <li class="mainLink"><a href="#">Support</a></li>
         </ul>
       </section>
 
@@ -45,7 +45,7 @@
       </ul>
     </nav>
     <p
-      class="p-6 block absolute text-[40px] leading-10 font-josefin text-white z-10 border-2 top-56 border-p-white mx-6 sm:w-[400px] sm:top-32"
+      class="p-6 block absolute text-[40px] leading-10 font-josefin text-white z-10 border-2 top-56 border-p-white mx-6 sm:w-[400px] sm:top-32 lg:ml-40 lg:text-[4.2rem] lg:leading-[1] lg:w-[38rem] lg:top-52 lg:p-8"
     >
       IMMERSIVE EXPERIENCES THAT DELIVER
     </p>
