a255a2163c423179132b784562134c5e9b37ab9b
LucSil <luciansilva351@gmail.com>
2022-04-11 08:38:29 +0100

    responsive tab design for VR.vue

--- src/components/children/VR.vue
+++ src/components/children/VR.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="sm:grid grid-cols-2">
     <picture>
       <source
         media="(min-width:500px)"
@@ -11,13 +11,15 @@
       />
       <img src="../../assets/images/pic/vr-desk.jpg" alt="Man with VR set" />
     </picture>
-    <div class="mt-10">
+    <div class="mt-10 sm:mt-2">
       <h1
-        class="text-[32px] leading-9 mb-6 font-josefin font-light text-center"
+        class="text-[32px] leading-9 mb-6 font-josefin font-light text-center sm:text-left sm:pl-6"
       >
         THE LEADER IN INTERACTIVE VR
       </h1>
-      <p class="font-alata px-6 font-normal text-p-darkGray text-center">
+      <p
+        class="font-alata px-6 font-normal text-p-darkGray text-center sm:text-justify"
+      >
         Founded in 2011, Loopstudios has been producing world-class virtual
         reality projects for some of the companies around the globe. Our
         award-winning creations have transformed businesses through digital
