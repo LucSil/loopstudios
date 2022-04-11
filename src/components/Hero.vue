c7f8406e74ec907da4a20e356f10c59bb4a7fb3a
LucSil <luciansilva351@gmail.com>
2022-04-11 08:32:16 +0100

    responsive tab design for Hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,7 +9,7 @@
         <div class="w-36">
           <img src="../assets/images/svg/logo.svg" alt="logo" />
         </div>
-        <div class="flex justify-center items-center">
+        <div class="flex justify-center items-center sm:hidden">
           <button v-if="!click" @click="toggle">
             <img
               src="../assets/images/svg/icon-hamburger.svg"
@@ -20,7 +20,19 @@
             <img src="../assets/images/svg/icon-close.svg" alt="close menu" />
           </button>
         </div>
+        <!-- Menu shown after mobile widths -->
+        <ul
+          class="sm:flex hidden text-white font-alata justify-between items-center"
+        >
+          <li class="mainLink imp"><a href="#">ABOUT</a></li>
+          <li class="mainLink imp"><a href="#">CAREERS</a></li>
+          <li class="mainLink imp"><a href="#">EVENTS</a></li>
+          <li class="mainLink imp"><a href="#">PRODUCTS</a></li>
+          <li class="mainLink"><a href="#">SUPPORT</a></li>
+        </ul>
       </section>
+
+      <!--  Mobile Menu -->
       <ul
         v-if="click"
         class="text-white font-josefin text-2xl absolute mt-40 z-30"
@@ -33,7 +45,7 @@
       </ul>
     </nav>
     <p
-      class="p-6 block absolute text-[40px] leading-10 font-josefin text-white z-10 border-2 top-50 top-56 border-p-white mx-6"
+      class="p-6 block absolute text-[40px] leading-10 font-josefin text-white z-10 border-2 top-56 border-p-white mx-6 sm:w-[400px] sm:top-32"
     >
       IMMERSIVE EXPERIENCES THAT DELIVER
     </p>
