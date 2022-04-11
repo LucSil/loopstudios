da14ce10d5dad915526f0d39b958f596624030cf
LucSil <luciansilva351@gmail.com>
2022-04-11 09:42:00 +0100

    responsive tab design for Footer.vue complete

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,50 +1,69 @@
 <template>
-  <section class="bg-black py-14 grid place-items-center">
-    <div class="w-40 mb-8">
-      <img src="../assets/images/svg/logo.svg" />
+  <section
+    class="bg-black py-14 grid place-items-center sm:flex sm:px-6 sm:justify-between sm:items-center sm:py-8"
+  >
+    <div>
+      <div class="w-40 mb-8 sm:mb-2">
+        <img src="../assets/images/svg/logo.svg" />
+      </div>
+      <ul
+        class="mb-10 sm:mb-0 text-white font-alata grid place-items-center sm:flex"
+      >
+        <li class="footLinkTxt impTwo">
+          <a href="#">About</a>
+        </li>
+        <li class="footLinkTxt impTwo">
+          <a href="#">Careers</a>
+        </li>
+        <li class="footLinkTxt impTwo">
+          <a href="#">Events</a>
+        </li>
+        <li class="footLinkTxt impTwo">
+          <a href="#">Products</a>
+        </li>
+        <li class="footLinkTxt">
+          <a href="#">Support</a>
+        </li>
+      </ul>
     </div>
-    <ul class="mb-10 text-white font-alata grid place-items-center">
-      <li class="footLinkTxt">
-        <a href="#">About</a>
-      </li>
-      <li class="footLinkTxt">
-        <a href="#">Careers</a>
-      </li>
-      <li class="footLinkTxt">
-        <a href="#">Events</a>
-      </li>
-      <li class="footLinkTxt">
-        <a href="#">Products</a>
-      </li>
-      <li class="footLinkTxt">
-        <a href="#">Support</a>
-      </li>
-    </ul>
-    <ul class="flex justify-between items-center w-40 mb-4">
-      <li>
-        <a href="#">
-          <img src="../assets/images/svg/icon-facebook.svg" alt="facebook" />
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src="../assets/images/svg/icon-twitter.svg" alt="twitter" />
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src="../assets/images/svg/icon-pinterest.svg" alt="pinterest" />
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src="../assets/images/svg/icon-instagram.svg" alt="instagram" />
-        </a>
-      </li>
-    </ul>
-    <p class="text-p-veryDarkGray font-alata">
-      &copy; 2022 Loopstudios. All rights reserved.
-    </p>
+    <div class="sm:justify-end">
+      <div class="flex justify-center sm:justify-end">
+        <ul class="flex justify-between items-center w-40 mb-4 sm:mb-2">
+          <li>
+            <a href="#">
+              <img
+                src="../assets/images/svg/icon-facebook.svg"
+                alt="facebook"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="../assets/images/svg/icon-twitter.svg" alt="twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img
+                src="../assets/images/svg/icon-pinterest.svg"
+                alt="pinterest"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img
+                src="../assets/images/svg/icon-instagram.svg"
+                alt="instagram"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <p class="text-p-veryDarkGray font-alata">
+        &copy; 2022 Loopstudios. All rights reserved.
+      </p>
+    </div>
   </section>
 </template>
 
