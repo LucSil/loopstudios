25792c13f9000bc97dcad924c1c4c6d1fb79c428
LucSil <luciansilva351@gmail.com>
2022-04-15 14:13:16 +0100

    simple animation for mobile menu created

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,24 +1,28 @@
 <template>
   <header class="relative grid">
-    <div
-      v-if="menu"
-      class="absolute inset-0 z-20 w-full h-screen bg-black"
-    ></div>
+    <transition name="fade">
+      <div
+        v-if="menu"
+        class="absolute inset-0 z-20 w-full h-screen bg-black"
+      ></div>
+    </transition>
     <nav class="absolute z-30 w-full mt-10 px-6 lg:px-40 lg:mt-16">
       <section class="flex justify-between items-center relative z-30">
         <div class="w-40">
           <img src="../assets/images/svg/logo.svg" alt="logo" />
         </div>
         <div class="flex justify-center items-center sm:hidden">
-          <button v-if="!click" @click="toggle">
-            <img
-              src="../assets/images/svg/icon-hamburger.svg"
-              alt="open menu"
-            />
-          </button>
-          <button v-if="click" @click="toggle">
-            <img src="../assets/images/svg/icon-close.svg" alt="close menu" />
-          </button>
+          <transition name="switch" mode="out-in">
+            <button v-if="!click" @click="toggle">
+              <img
+                src="../assets/images/svg/icon-hamburger.svg"
+                alt="open menu"
+              />
+            </button>
+            <button v-else @click="toggle">
+              <img src="../assets/images/svg/icon-close.svg" alt="close menu" />
+            </button>
+          </transition>
         </div>
         <!-- Menu shown after mobile widths -->
         <ul
@@ -63,16 +67,18 @@
       </section>
 
       <!--  Mobile Menu -->
-      <ul
-        v-if="click"
-        class="text-white font-josefin text-2xl absolute mt-40 z-30"
-      >
-        <li class="mainLink"><a href="#">ABOUT</a></li>
-        <li class="mainLink"><a href="#">CAREERS</a></li>
-        <li class="mainLink"><a href="#">EVENTS</a></li>
-        <li class="mainLink"><a href="#">PRODUCTS</a></li>
-        <li class="mainLink"><a href="#">SUPPORT</a></li>
-      </ul>
+      <transition name="menu">
+        <ul
+          v-if="click"
+          class="text-white font-josefin text-2xl absolute mt-40 z-30"
+        >
+          <li class="mainLink"><a href="#">ABOUT</a></li>
+          <li class="mainLink"><a href="#">CAREERS</a></li>
+          <li class="mainLink"><a href="#">EVENTS</a></li>
+          <li class="mainLink"><a href="#">PRODUCTS</a></li>
+          <li class="mainLink"><a href="#">SUPPORT</a></li>
+        </ul>
+      </transition>
     </nav>
     <p
       class="p-6 block absolute text-[40px] leading-10 font-josefin text-white z-10 border-2 top-56 border-p-white mx-6 sm:w-[400px] sm:top-32 lg:ml-40 lg:text-[4.2rem] lg:leading-[1] lg:w-[38rem] lg:top-52 lg:p-8"
@@ -117,3 +123,33 @@ export default {
   },
 };
 </script>
+<style>
+.switch-enter-from,
+.switch-leave-to {
+  opacity: 0;
+}
+.switch-enter-active,
+.switch-leave-active {
+  transition: all 0.2s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(500px);
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
+}
+.menu-enter-from,
+.menu-leave-to {
+  transform: translateX(500px);
+  opacity: 0;
+}
+.menu-enter-active {
+  transition: all 0.5s ease;
+}
+.menu-leave-active {
+  transition: all 0.2s ease;
+}
+</style>
